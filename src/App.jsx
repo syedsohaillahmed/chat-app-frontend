13b8ab499d475e32f1ffac87ebfc2b2a0f10b606
syedsohaillahmed <syedsohaillahmed19@gmail.com>
2024-10-08 17:54:33 +0530

    Added routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,38 @@
 import React, { lazy } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ProtectedRoutes from "./components/auth/ProtectedRoutes";
 
 const Home = lazy(() => import("./pages/Home"));
 const Chat = lazy(() => import("./pages/Chat"));
 const Group = lazy(() => import("./pages/Group"));
-const Login = lazy(()=> import("./pages/Login"))
+const Login = lazy(() => import("./pages/Login"));
 
 const App = () => {
+  const user = true;
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/chat/:chatId" element={<Chat />} />
-        <Route path="/group" element={<Group />} />
+        //protected routes
+        <Route element={<ProtectedRoutes user={user} />}>
+          <Route path="/" element={<Home />} />
+
+          <Route path="/chat/:chatId" element={<Chat />} />
+          <Route path="/group" element={<Group />} />
+        </Route>
+
+        //umprotected routes
+        <Route
+          path="/login"
+          element={
+            <ProtectedRoutes user={!user} redirectTo="/">
+              <Login />
+            </ProtectedRoutes>
+          }
+        />
+
+
+        <Route path="*" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
